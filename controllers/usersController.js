00fe6eae61b97f7aa55d0cc6a864a8a7d97b2b8c
George Cavazos <cavazosgeorge@yahoo.com>
2021-10-25 18:18:01 -0400

    Updated controller.js comments

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,10 +3,12 @@ const Users = require("../api/users/model");
 // METHOD(GET) GET ALL USERS => /API/USERS
 exports.getUsers = async (req, res) => {
   Users.find()
+    // .THEN => WE RECIEVED WHAT IS SUPPOSED TO BE THE CORRECT USER DATA
     .then((users) => {
       res.json(users);
       console.log(users);
     })
+    // ERROR RESPONSE FOR DEVELOPER => 500 INTERNAL SERVER ERROR
     .catch((err) => {
       res.status(500).json({
         message: "error getting users",
@@ -19,14 +21,19 @@ exports.getUsers = async (req, res) => {
 // METHOD(GET) GET USER BY ID => /API/USERS/:ID
 exports.getUser = async (req, res) => {
   Users.findById(req.params.id)
+    // .THEN WE RECIEVED WHAT IS SUPPOSED TO BE THE CORRECT USER DATA
     .then((user) => {
+      // IF => ASSUMING THE CLIENT ENTERED THE USER INFORMATION INCORRECTLY
       if (!user) {
+        // ERROR RESPONSE FOR CLIENT => 404 CLIENT SIDE ERROR
         res.status(404).json({
           message: "The user with the specified ID does not exist",
         });
       }
+      // IF NOT => ASSUMING THE INFORMATION THE CLIENT ENTERED IS CORRECT, WE RETURN THE USER DATA
       res.json(user);
     })
+    // ERROR RESPONSE FOR DEVELOPER => 500 INTERNAL SERVER ERROR
     .catch((err) => {
       res.status(500).json({
         message: "error getting user",
